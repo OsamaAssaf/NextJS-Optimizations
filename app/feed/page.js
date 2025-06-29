6eb7568ace49f73ccbf46b8f64d095228ab13b08
Osama Assaf <96818454+OsamaAssaf@users.noreply.github.com>
2025-06-29 16:04:38 +0300

    commit

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,5 +1,18 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+
+// export const metadata = {
+//   title: "Browse all our X posts.",
+//   description: "Browse all our posts.",
+// };
+
+export async function generateMetadata() {
+  const posts = await getPosts();
+  return {
+    title: `Browse all our ${posts.length} posts.`,
+    description: "Browse all our posts.",
+  };
+}
 
 export default async function FeedPage() {
   const posts = await getPosts();
